2e578d8263929bde2458422f9334e3463ab47bd3
xernyuii <hfutxc.xern@gmail.com>
2018-02-04 15:17:29 +0800

    fin dir

--- src/dir.c
+++ src/dir.c
@@ -27,11 +27,20 @@ int main(int argc, char**argv){
         strcpy(lines[1], "./");
         //printf("hi\n");
     }
+    else if(lines[1][strlen(lines[1])-1]!='/'){
+        strcat(lines[1],"/");
+    }
     //printf("hi2\n");
     if((fd=opendir(lines[1]))==NULL){
         perror("fail!");
         exit(0);
     }
+    //printf("%s\n", temp);
+    printf("%s 目录\n", lines[1]);
+    int file_num=0;
+    int dir_num=0;
+    long long file_size=0;
+    long long dir_size=0;
     while((fp=readdir(fd))!=NULL){
         strcpy(temp, lines[1]);
         //if(strcmp(temp,NULL)==0){
@@ -44,15 +53,22 @@ int main(int argc, char**argv){
             exit(0);
         }
 
-        printf("\t%.12s ", 4 + ctime(&buf.st_mtime));
+        printf("%.12s ", 4 + ctime(&buf.st_mtime));
         if(strcmp(fp->d_name,".")==0 || strcmp(fp->d_name,"..")==0){
-            printf("\t<DIR> ");
+            printf("        <DIR>");
+            dir_num++;
+            dir_size+=buf.st_size;
         }
-        else
-            printf("\t%lld", buf.st_size);
+        else{
+            printf("\t%10lld", buf.st_size);
+            file_num++;
+            file_size+=buf.st_size;
+        }
+            
         printf("   %s\n", fp->d_name);
 
     }
-
+    printf("%12d 个文件 %12lld 字节\n",file_num,file_size);
+    printf("%12d 个目录 %12lld 字节\n",dir_num,dir_size);
     return 0;
 }
